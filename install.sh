618ee50cb12b5bbbc4855470e6728a626e7edafe
FANG <fabius8@163.com>
2023-02-21 06:39:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 curl -s https://api.nodes.guru/swap4.sh | bash
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt update
-sudo apt install curl make clang pkg-config libssl-dev build-essential git jq nodejs -y < "/dev/null"
+sudo apt install curl make clang pkg-config libssl-dev build-essential git jq nodejs -y
 sudo apt install npm 
 sudo npm install -g ironfish
 ironfish stop
