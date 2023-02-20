4e5b12ca6c0dd019273f0fedab91e1983a6ea31e
FANG <fabius8@163.com>
2023-02-20 22:00:42 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -s https://api.nodes.guru/swap8.sh | bash
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt update
+sudo apt install curl make clang pkg-config libssl-dev build-essential git jq nodejs -y < "/dev/null"
+sudo apt install npm 
+sudo npm install -g ironfish
+ironfish stop
+echo -e "Y\n" | ironfish chain:download
+(ironfish start 2>/dev/null 1>/dev/null &)
+ironfish status
